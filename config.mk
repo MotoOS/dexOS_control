e0d28f13aec8a63efe48d5fa2fe340608548faeb
seba3567 <sebastian26481@gmail.com>
2020-05-28 17:10:59 -0400

    init

---
+++ config.mk
@@ -0,0 +1,18 @@
+# DexOS center control
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_PATH := dexOS/control
+
+ifneq ($(DexOS),true)
+include vendor/bootanimations/config.mk
+endif
+
+ifneq ($(DexOS),true)
+include vendor/prebuilt/config.mk
+endif
+
+ifneq ($(DexOS),true)
+include vendor/sound/config.mk
+endif
